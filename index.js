0e79dcb07a3e13e21a4f090b7d2981ef6eaec0f8
GustavoHeimburg <gutogustavogu@gmail.com>
2023-10-17 01:03:41 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-const jump = () =>{
+const jump = () => {
 mario.classList.add('jump');
 setTimeout(() =>{
     mario.classList.remove('jump');
